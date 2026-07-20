0b5a7164571b2b2974c9ae69fda3cbf51729efab
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-07-21 00:14:37 +0200

    Add variables for GitHub Pages module

---
+++ modules/github-pages/variables.tf
@@ -0,0 +1,5 @@
+variable "github_owner" { type = string }
+variable "repo_name" { type = string }
+variable "pages_hostname" { type = string }
+variable "github_pages_cname_target" { type = string }
+variable "cloudflare_zone_id" { type = string }
