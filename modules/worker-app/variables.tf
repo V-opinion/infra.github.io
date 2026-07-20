b0003a3e2af7df39722ed06483731181e9fc9f71
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-07-21 00:11:44 +0200

    Add variables for Cloudflare worker configuration

---
+++ modules/worker-app/variables.tf
@@ -0,0 +1,8 @@
+variable "cloudflare_account_id" { type = string }
+variable "zone_id" { type = string }
+variable "zone_name" { type = string }
+variable "worker_name" { type = string }
+variable "worker_hostname" { type = string }
+variable "worker_script_path" { type = string }
+variable "kv_state_namespace_id" { type = string }
+variable "kv_content_namespace_id" { type = string }
