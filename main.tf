a5d5585688166fba3e59aab909626ce1fc36c84d
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-07-20 23:57:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,3 +36,27 @@ module "apex_redirect" {
   apex_hostname      = var.zone_name
   www_hostname       = var.pages_hostname
 }
+resource "cloudflare_ruleset" "this" {
+  zone_id     = var.cloudflare_zone_id
+  name        = "apex-to-www"
+  description = "Redirect apex to www"
+  kind        = "zone"
+  phase       = "http_request_dynamic_redirect"
+
+  rules {
+    enabled     = true
+    description = "Redirect apex host to www"
+    expression  = "(http.host eq \"${var.apex_hostname}\")"
+    action      = "redirect"
+
+    action_parameters {
+      from_value {
+        status_code = 301
+        target_url {
+          expression = "concat(\"https://${var.www_hostname}\", http.request.uri.path)"
+        }
+        preserve_query_string = true
+      }
+    }
+  }
+}
